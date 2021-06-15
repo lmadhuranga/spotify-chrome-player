44c728b58c1af34826fc4fe61813946d93261c51
Madhuragna Senadheera <lilan.maduranga@gmail.com>
2021-06-15 23:58:44 +0530

    added authentication for the application

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+		var token = '';
     var settings = {
 			"url": "",
 			"method": "POST",
@@ -6,11 +7,12 @@ $(document).ready(function(){
 			"headers": {
 				"Accept": "application/json",
 				"Content-Type": "application/json",
-				"Authorization": "Bearer BQAAI4n7HvUQpQaJlBCSJL837R__PAHz1aiIkajUZRrU0J1D4sykg8YB0uRyYdVPHcZ2xyonXn2EMWLMrYiuRC3JC0voSnbmoYQ2ld0Ds9SAae1aS1xlGhr1sv6Ilv7NnjlOswzb6z-i4yZPG-YljN3vigGEyZQnsTADYuYhoNEAHnjjdsywWkcuUCs_UnnWoW45h_B4BIkr3cwhJILo3QELcmXzf2AewhtEfcMUEwo0JiNrLkbuV81WyUNkvNrBtMNLTHUbAGTOxi0o20EapYqhDiP9e5GbzBacGLYqZf5O"
+				"Authorization": ``
 			},
 		};
-		function submitData(_settings) {
-			$.ajax(_settings).done(function (response) {
+		function submitData(_settings) {  
+			console.log(`msg_ _settings`,settings);
+			$.ajax(settings).done(function (response) {
 				console.log(response);
 			});
 		}
@@ -55,5 +57,97 @@ $(document).ready(function(){
 			submitData(settings);
 			console.log(`msg_ vol 100 `);  
     });
+
+ 
+
+		var stateKey = 'spotify_auth_state';
+
+		/**
+		 * Obtains parameters from the hash of the URL
+		 * @return Object
+		 */
+		function getHashParams() {
+			var hashParams = {};
+			var e, r = /([^&;=]+)=?([^&;]*)/g,
+					q = window.location.hash.substring(1);
+			while ( e = r.exec(q)) {
+					hashParams[e[1]] = decodeURIComponent(e[2]);
+			}
+			return hashParams;
+		}
+
+		/**
+		 * Generates a random string containing numbers and letters
+		 * @param  {number} length The length of the string
+		 * @return {string} The generated string
+		 */
+		function generateRandomString(length) {
+			var text = '';
+			var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+			for (var i = 0; i < length; i++) {
+				text += possible.charAt(Math.floor(Math.random() * possible.length));
+			}
+			return text;
+		};
+
+		// var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+		//     userProfileTemplate = Handlebars.compile(userProfileSource),
+		//     userProfilePlaceholder = document.getElementById('user-profile');
+
+		//     oauthSource = document.getElementById('oauth-template').innerHTML,
+		//     oauthTemplate = Handlebars.compile(oauthSource),
+		//     oauthPlaceholder = document.getElementById('oauth');
+
+		var params = getHashParams();
+
+		var access_token = params.access_token,
+				state = params.state,
+				storedState = localStorage.getItem(stateKey);
+
+		if (access_token && (state == null || state !== storedState)) {
+			window.location = '?msg=There was an error during the authentication'
+		} else {
+			localStorage.removeItem(stateKey);
+			if (access_token) { 
+				settings['headers']['Authorization'] = `Bearer ${access_token}`; 
+				token = access_token;
+				$.ajax({
+						url: 'https://api.spotify.com/v1/me',
+						headers: {
+							'Authorization': 'Bearer ' + access_token
+						},
+						success: function(response) {
+							// userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+
+							$('#login').hide();
+							$('#loggedin').show();
+						}
+				});
+			} else {
+					$('#login').show();
+					$('#loggedin').hide();
+			}
+
+			document.getElementById('login-button').addEventListener('click', function() {
+
+				var client_id = '8d0eabf6d4a647f1a6113a3e3b07168d'; // Your client id
+				var redirect_uri = 'http://127.0.0.1:5500'; // Your redirect uri
+
+				var state = generateRandomString(16);
+
+				localStorage.setItem(stateKey, state);
+				var scope = 'user-read-private user-read-email user-modify-playback-state playlist-modify-public';
+
+				var url = 'https://accounts.spotify.com/authorize';
+				url += '?response_type=token';
+				url += '&client_id=' + encodeURIComponent(client_id);
+				url += '&scope=' + encodeURIComponent(scope);
+				url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+				url += '&state=' + encodeURIComponent(state);
+
+				window.location = url;
+			}, false);
+		}
     
 });
